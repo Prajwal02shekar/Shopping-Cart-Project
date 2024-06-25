0e17efed9ff8ad331dbde74fccc812c583a8f986
prajwal02shekar <prajwal02shekar@gmail.com>
2024-06-25 21:02:42 +0530

    updated

--- login.js
+++ login.js
@@ -50,3 +50,40 @@ ele.addEventListener("submit", (e) => {
 })
 
 // --------Login ends here---------
+
+
+//!  -------------------------
+
+// let shop = document.getElementById("shop");
+// let fetchdata = async function () {
+//     let res = await fetch("https://fakestoreapi.com/products")
+//     data = await res.json()
+//     return data;
+// }
+// fetchdata();
+// console.log(fetchdata());
+// fetchdata().then((d) => {
+//     console.log(d);
+
+//     let generateShop = () => {
+//         return (shop.innerHTML = d.map((item) => {
+//             return `<div id=product-id-${item.id} class="item">
+//             <div class="img-box"><img src="${item.image}" alt=""></div>
+//             <div class="details">
+//                 <h3>${item.title}</h3>
+//                 <p>${item.description.slice(0, 100)}</p>
+//                 <p>Rating: ${item.rating.rate}/5 <i class="bi bi-star"></i></p>
+//                 <div class="price-quantity">
+//                     <h2>&#8377; ${item.price}</h2>
+//                     <div class="buttons">
+//                         <i onclick="decrement(${item.id})" class="bi bi-dash-square"></i>
+//                         <div id=${item.id} class="quantity">0</div>
+//                         <i onclick="increment(${item.id})"  class="bi bi-plus-square"></i>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>`
+//         }).join(""))
+//     }
+//     generateShop()
+// })
--- main.js
+++ main.js
@@ -3,54 +3,52 @@ let userName = localStorage.getItem("user-name")
 loginBtn.innerHTML = `Logged in as ${userName}`
 
 
+
+//todo ------------------------------------------------------
 let shop = document.getElementById("shop");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
 let fetchdata = async function () {
-    let res = await fetch("https://fakestoreapi.com/products")
-    data = await res.json()
+    let res = await fetch("https://fakestoreapi.com/products");
+    let data = await res.json();
     return data;
-}
-fetchdata();
-console.log(fetchdata());
+};
+
 fetchdata().then((d) => {
     console.log(d);
 
     let generateShop = () => {
-        return (shop.innerHTML = d.map((item) => {
-            return `<div id=product-id-${item.id} class="item">
-            <div class="img-box"><img src="${item.image}" alt=""></div>
+        shop.innerHTML = d.map((item) => {
+            let { id, image, title, price } = item;
+            let search = basket.find((y) => y.id === id) || [];
+            return `<div id=product-id-${id} class="item">
+            <div class="img-box"><img src="${image}" alt=""></div>
             <div class="details">
-                <h3>${item.title}</h3>
-                <p>${item.description.slice(0, 100)}</p>
-                <p>Rating: ${item.rating.rate}/5 <i class="bi bi-star"></i></p>
+                <h3>${title}</h3>
                 <div class="price-quantity">
-                    <h2>&#8377; ${item.price}</h2>
+                    <h2>&#8377; ${price}</h2>
                     <div class="buttons">
-                        <i onclick="decrement(${item.id})" class="bi bi-dash-square"></i>
-                        <div id=${item.id} class="quantity">0</div>
-                        <i onclick="increment(${item.id})"  class="bi bi-plus-square"></i>
+                        <i onclick="decrement(${id})" class="bi bi-dash-square"></i>
+                        <div id="${id}" class="quantity">${search.item === undefined ? 0 : search.item}</div>
+                        <i onclick="increment(${id})" class="bi bi-plus-square"></i>
                     </div>
                 </div>
             </div>
-        </div>`
-        }).join(""))
-    }
-    generateShop()
-})
-
-
-let basket = {
-    id:"abb",
-    item:1
-}
-
+        </div>`;
+        }).join("");
+    };
+    
+    generateShop();
+});
 
 let increment = (id) => {
     let selectedItem = id;
-    let search = basket.find((x) => x.id === selectedItem.id);
+    let search = basket.find((x) => x.id === selectedItem);
 
     if (search === undefined) {
         basket.push({
-            id: selectedItem.id,
+            id: selectedItem,
             item: 1,
         });
     } else {
@@ -58,31 +56,43 @@ let increment = (id) => {
     }
 
     console.log(basket);
-    update(selectedItem.id);
-   
+    update(selectedItem);
+    localStorage.setItem("data", JSON.stringify(basket));
 };
+
 let decrement = (id) => {
     let selectedItem = id;
-    let search = basket.find((x) => x.id === selectedItem.id);
+    let search = basket.find((x) => x.id === selectedItem);
 
     if (search === undefined) return;
     else if (search.item === 0) return;
     else {
         search.item -= 1;
     }
-    update(selectedItem.id);
+
+    update(selectedItem);
     basket = basket.filter((x) => x.item !== 0);
-    // console.log(basket);
-    
+    console.log(basket);
+    localStorage.setItem("data", JSON.stringify(basket));
 };
+
 let update = (id) => {
-    console.log(id);
-
     let search = basket.find((x) => x.id === id);
-    // console.log(search.item);
+    let quantityElement = document.getElementById(id);
 
-   
+    if (quantityElement) {
+        quantityElement.innerHTML = search.item;
+        // calculation();
+    } else {
+        console.error(`Element with ID ${id} not found`);
+    }
 };
 
+// let calculation = () => {
+//   let cartIcon = document.getElementById("cartAmount");
+//   cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+// };
+
+// calculation();
 
 
